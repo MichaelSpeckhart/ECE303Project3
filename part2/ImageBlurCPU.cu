6f0b88ef136c2473ea04cd183e3450942c5523eb
Luke Hale <lsh224@ece-hpc03.cc.lehigh.edu>
2024-03-04 11:42:12 -0500

    adding info about the data format

--- part2/ImageBlurCPU.cu
+++ part2/ImageBlurCPU.cu
@@ -2,9 +2,13 @@
 #include "timer.h"
 
 //the first step is to read in the MINST data set
+// Each file has 1000 training examples. Each training example is of size 28x28 pixels. 
+// The pixels are stored as unsigned chars (1 byte) and take values from 0 to 255. 
+// The first 28x28 bytes of the file correspond to the first training example, the next 28x28 bytes correspond to the next example and so on.
 int main(){
-    
+
 }
+//this sudo code is for each image being a 1d array
 // for (blurrow = -BLUR_SIZE to BLUR_SIZE){
 //     for (blurcol = -BLUR_SIZE to BLUR_SIZE){
 //     currow = row + blurrow;
