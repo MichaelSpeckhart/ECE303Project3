b907cb78ca4be8fb0ac1f22ed3d25885a94e858e
Luke Hale <lsh224@ece-hpc03.cc.lehigh.edu>
2024-03-05 18:11:13 -0500

    stupid parsing

--- part2/ImageBlurCPU.cu
+++ part2/ImageBlurCPU.cu
@@ -1,12 +1,26 @@
 #include "files.h"
 #include "timer.h"
+#include <iostream>
+#include <iomanip>
+
 
 //the first step is to read in the MINST data set
 // Each file has 1000 training examples. Each training example is of size 28x28 pixels. 
 // The pixels are stored as unsigned chars (1 byte) and take values from 0 to 255. 
 // The first 28x28 bytes of the file correspond to the first training example, the next 28x28 bytes correspond to the next example and so on.
 int main(){
-
+    unsigned char letters[1000][28*28];
+    
+    read_values_from_file("data/data2.txt", letters,1000);
+    for(size_t x = 0 ; x < 10;x++){
+        for(size_t i = 0; i < 28;i++){
+            for(size_t j = 0; j<28;j++){
+                std::cout << std::setw(3) << static_cast<int>(letters[0][i * 28 + j]) << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
 }
 //this sudo code is for each image being a 1d array
 // for (blurrow = -BLUR_SIZE to BLUR_SIZE){
--- part2/files.h
+++ part2/files.h
@@ -5,10 +5,24 @@
 #include <cstdint>
 #include <string>
 
-void read_values_from_file(const char * file, float * data, size_t size) {
-    std::ifstream values(file, std::ios::binary);
-    values.read(reinterpret_cast<char*>(data), size);
-    values.close();
+// void read_values_from_file(const char * file, float * data, size_t size) {
+//     std::ifstream values(file, std::ios::binary);
+//     values.read(reinterpret_cast<char*>(data), size);
+//     values.close();
+// }
+
+void read_values_from_file(const char *file, unsigned char data[][28*28], size_t num_values_to_read) {
+    std::ifstream fid(file, std::ios::binary);
+    if (!fid.is_open()) {
+        std::cout << "Error opening file." << std::endl;
+        return;
+    }
+    
+    for (size_t i = 0; i < num_values_to_read; ++i) {
+        fid.read(reinterpret_cast<char*>(data[i]), sizeof(unsigned char) * 28 * 28);
+    }
+    
+    fid.close();
 }
 
 void write_values_to_file(const char * file, float * data, size_t size) {
