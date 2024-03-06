6e831ce15c43758fee67987c3ab1102d38d77a90
Luke Hale <lsh224@ece-hpc03.cc.lehigh.edu>
2024-03-06 13:45:14 -0500

    more work

--- part2/ImageBlurGPU.cu
+++ part2/ImageBlurGPU.cu
@@ -4,7 +4,7 @@
 #include <iomanip>
 const size_t BLUR_SIZE = 1;
 const size_t IMAGE_SIZE = 28;
-const size_t NUMBER_IMAGES = 1;
+const size_t NUMBER_IMAGES = 1000;
 
 __global__ void blur_kernel(unsigned char *device_in_buffer,unsigned char *device_out_buffer)
 {
@@ -14,8 +14,8 @@ __global__ void blur_kernel(unsigned char *device_in_buffer,unsigned char *devic
 
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = gridDim.x * blockDim.x;
-//  for (int i = idx; i < n; i += stride) {
-    for(size_t image_number = idx; image_number < NUMBER_IMAGES; stride++) {
+    //increment by stride
+    for(size_t image_number = idx; image_number < NUMBER_IMAGES; image_number += stride) {
         // For each pixel
         for(size_t i = 0; i < IMAGE_SIZE; i++) {
             for(size_t j = 0; j < IMAGE_SIZE; j++) {
@@ -47,12 +47,10 @@ int main(){
 
     size_t bytes = NUMBER_IMAGES * IMAGE_SIZE * IMAGE_SIZE * sizeof(unsigned char);
     // Allocate memory for input and output buffers
-    unsigned char* inBuf = (unsigned char*)malloc(bytes);
-    unsigned char* outBuf = (unsigned char*)malloc(bytes);
+    unsigned char* hostBuf = (unsigned char*)malloc(bytes);
 
     // Allocate memory for input and output images
-    unsigned char (*inImage)[IMAGE_SIZE][IMAGE_SIZE] = (unsigned char (*)[IMAGE_SIZE][IMAGE_SIZE])inBuf;
-    unsigned char (*outImage)[IMAGE_SIZE][IMAGE_SIZE] = (unsigned char (*)[IMAGE_SIZE][IMAGE_SIZE])outBuf;
+    unsigned char (*inImage)[IMAGE_SIZE][IMAGE_SIZE] = (unsigned char (*)[IMAGE_SIZE][IMAGE_SIZE])hostBuf;
     
     read_values_from_file("data/data3.txt", inImage,NUMBER_IMAGES);
     for(size_t x = 0 ; x < 1;x++){
@@ -67,11 +65,9 @@ int main(){
     //get the memory onto the GPU
     unsigned char *device_in_buffer;
     cudaMalloc(&device_in_buffer, bytes);
-    //cudaMemcpy(device_in_buffer, inBuf, bytes, cudaMemcpyHostToDevice);
 
     unsigned char *device_out_buffer;
     cudaMalloc(&device_out_buffer, bytes);
-    cudaMemcpy(device_out_buffer, inBuf, bytes, cudaMemcpyHostToDevice);
   
     int threads_per_block = 512;
     int deviceId;
@@ -82,28 +78,18 @@ int main(){
     
     int number_of_blocks = 32 * numberOfSMs;
 
-
+    cudaMemcpy(device_in_buffer, hostBuf, bytes, cudaMemcpyHostToDevice);
     //for each input image
     StartTimer();
-    for(size_t many = 0; many <10;many++){
-        std::cerr << "HI!" << std::endl;
-        //call my kernel here
-        cudaMemcpy(device_in_buffer, inBuf, bytes, cudaMemcpyHostToDevice);
-        std::cerr << "Hello" << std::endl;
+    for (size_t blur = 0; blur < 10; blur++) {
         blur_kernel<<<number_of_blocks, threads_per_block>>>(device_in_buffer, device_out_buffer);
-        std::cerr << "goodBye" << std::endl;
-        cudaMemcpy(outBuf, device_out_buffer, bytes, cudaMemcpyDeviceToHost);
-        std::cerr << "bye" << std::endl;
-                // Copy outImage to inImage
-        for (size_t i = 0; i < NUMBER_IMAGES; i++) {
-            for (size_t j = 0; j < IMAGE_SIZE; j++) {
-                for (size_t k = 0; k < IMAGE_SIZE; k++) {
-                    inImage[i][j][k] = outImage[i][j][k];
-                }
-            }
-        }
+        cudaDeviceSynchronize();
+        unsigned char *temp = device_in_buffer;
+        device_in_buffer = device_out_buffer;
+        device_out_buffer = temp;
     }
     std::cout << GetTimer()/NUMBER_IMAGES/10 << " ms per image average" << std::endl;
+    cudaMemcpy(hostBuf, device_out_buffer, bytes, cudaMemcpyDeviceToHost);
 
     for(size_t x = 0 ; x < 1;x++){
         for(size_t i = 0; i < IMAGE_SIZE;i++){
@@ -115,8 +101,7 @@ int main(){
         std::cout << std::endl;
     }
     write_values_to_file("dataOut/data3.txt", inImage,NUMBER_IMAGES);
-    free(inBuf);
-    free(outBuf);
+    free(hostBuf);
     //CUDA FREE
     cudaFree( device_in_buffer );
     cudaFree( device_out_buffer );
